60ba33c73d09cacac55a0175404f3640cc75a8be
Ronaldmupangurit <85442718+Ronaldmupangurit@users.noreply.github.com>
2021-06-25 23:13:54 +0200

    Add files via upload

---
+++ color.js
@@ -0,0 +1,11 @@
+function myFuction()
+        {
+            var change = document.getElementsByClassName("color");
+        
+        for (var i = 0; i <change.length;i++)
+        {
+            change[i].style.backgroundColor = "black";
+            change[i].style.backgroundColor = "white";
+        }
+        
+        }
